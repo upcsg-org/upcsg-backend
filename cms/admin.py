e5336091366355247ccf829cb0ef6c37f67959a4
Maxell Milay <milaymaxell@gmail.com>
2025-04-24 02:56:02 +0800

    updated admin

--- cms/admin.py
+++ cms/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Article, Event, Scholarship, Internship, Announcement
 
-# Register your models here.
+admin.site.register(Article)
+admin.site.register(Event)
+admin.site.register(Scholarship)
+admin.site.register(Internship)
+admin.site.register(Announcement)
--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from .models import User
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register the User model
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'phone_number')
+    search_fields = ('username', 'email', 'first_name', 'last_name', 'phone_number')
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'image_url', 'bio', 'phone_number', 'date_of_birth')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
+# Register the User model with custom admin
+admin.site.register(User, CustomUserAdmin)
