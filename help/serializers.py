c678ca6ab94789872d9bdcac50c23f51471b0c24
Maxell Milay <milaymaxell@gmail.com>
2025-05-03 14:13:39 +0800

    added backend email sending logic adn concern creation

--- help/serializers.py
+++ help/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Concern
-
+from user.models import User
 class ConcernSerializer(serializers.ModelSerializer):
     class Meta:
         model = Concern
         fields = '__all__'
+
+class EmailFormSerializer(serializers.Serializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False, allow_null=True)
+    name = serializers.CharField(max_length=255)
+    email = serializers.EmailField()
+    content = serializers.CharField()
