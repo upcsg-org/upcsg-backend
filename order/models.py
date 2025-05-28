d0a36b8643d048977f9a1a4097739ec7cccc2ee0
Maxell Milay <milaymaxell@gmail.com>
2025-05-29 01:16:09 +0800

    implemented the order feature

--- order/models.py
+++ order/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from user.models import User
-from merch.models import Merch
+from merch.models import MerchVariant
 
 class Order(models.Model):
     class PaymentMethod(models.TextChoices):
-        GCASH = 'gcash'
-        BANK_TRANSFER = 'bank_transfer'
+        ONLINE = 'online'
+        CASH = 'cash'
 
     class OrderStatus(models.TextChoices):
         PENDING = 'pending'
@@ -15,6 +15,7 @@ class Order(models.Model):
 
     buyer = models.ForeignKey(User, on_delete=models.CASCADE)
     payment_method = models.CharField(max_length=255, choices=PaymentMethod.choices)
+    proof_of_payment = models.URLField(null=True, blank=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     date_created = models.DateTimeField(auto_now_add=True)
     date_paid = models.DateTimeField(null=True, blank=True)
@@ -25,9 +26,9 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    merch = models.ForeignKey(Merch, on_delete=models.CASCADE)
+    merch_variant = models.ForeignKey(MerchVariant, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     subtotal_price = models.DecimalField(max_digits=10, decimal_places=2)
     
     def __str__(self):
-        return f"OrderItem {self.id} - {self.merch.name}"
+        return f"OrderItem {self.id} - {self.merch_variant.name}"
--- order/urls.py
+++ order/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import OrderView, OrderItemView
+from .views import OrderView, OrderItemView, ManageOrderView, ManageOrderItemView
 
 urlpatterns = [
-    path('order/', OrderView.as_view({'get': 'list', 'post': 'create'}), name='order'),
-    path('order/<int:pk>/', OrderView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='order_detail'),
+    path('', OrderView.as_view({'get': 'list', 'post': 'create'}), name='order'),
+    path('<int:pk>/', OrderView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='order_detail'),
     path('order-item/', OrderItemView.as_view({'get': 'list', 'post': 'create'}), name='order_item'),
     path('order-item/<int:pk>/', OrderItemView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='order_item_detail'),
+    path('manage/', ManageOrderView.as_view({'get': 'list', 'post': 'create'}), name='manage_order'),
+    path('manage/<int:pk>/', ManageOrderView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='manage_order_detail'),
+    path('manage/order-item/', ManageOrderItemView.as_view({'get': 'list', 'post': 'create'}), name='manage_order_item'),
+    path('manage/order-item/<int:pk>/', ManageOrderItemView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='manage_order_item_detail'),
 ]
